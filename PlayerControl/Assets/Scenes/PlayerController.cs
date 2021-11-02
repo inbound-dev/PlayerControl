a2d38cb18d34974c084d1c021783c9ebb17734f0
Jordan Hamilton <jordanh6489@hotmail.com>
2021-11-02 15:08:04 -0400

    Completed Unity Unit 6

--- PlayerControl/Assets/Scenes/PlayerController.cs
+++ PlayerControl/Assets/Scenes/PlayerController.cs
@@ -10,19 +10,20 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float turnSpeed = 50f;
 
     private float horizontalInput;
+    private float forwardInput;
 
     [SerializeField] Vector3 offset = new Vector3(0, 5, -7);
 
     void Start()
     {
-        transform.position = player.transform.position = offset;
+        transform.position = car.transform.position = offset;
     }
     void LateUpdate()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
-        transform.translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        transform.rotate(Vector3.up * Time.deltaTime * turnspeed * horizontalInput);
+        car.transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        car.transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
